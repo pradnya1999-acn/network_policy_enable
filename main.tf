fae2c0ac770bd5a26d694ec26566133b3868a3d8
pradnya1999-acn <159138875+pradnya1999-acn@users.noreply.github.com>
2024-04-08 16:21:59 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+#   version = "= 3.55.0"
+  credentials = var.sa_key
+  project = "parabolic-base-409505"
+}
+ 
+variable "sa_key" {
+  default =  ""
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+
+
+    network_policy {
+    enabled  = true
+    provider = "CALICO"
+  }
+}
